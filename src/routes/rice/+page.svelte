906437414cb2b1dddcc7f0d255bf6c8f1a53692a
Evan Alvarez <evanalvarez@tutanota.com>
2023-03-09 20:47:30 -0600

    I finally fixed the image issue

--- src/routes/rice/+page.svelte
+++ src/routes/rice/+page.svelte
@@ -27,7 +27,7 @@
 
       <div class="hero min-h-50% bg-base-100">
         <div class="hero-content flex-col lg:flex-row">
-          <img src="/static/rice-ibrahim.png" class="max-w-sm rounded-lg shadow-2xl rice-images" />
+          <img src="https://raw.githubusercontent.com/evfeal/rice-ibrahim/main/assets/screenshot_01.png" class="max-w-sm rounded-lg shadow-2xl rice-images" />
         <div>
           <h1 class="text-5xl font-bold">Ibrahim (Sway)</h1>
           <p class="py-6">This is my first decent rice, it is on the sway window manager <br> (wayland), and focuses on appearance over minimalism.</p>
